d8ddb548c6dcbb4ff88027047e3b7a47df4de412
Prasad-12345 <prasadsomvanshi471@gmail.com>
2022-07-21 20:33:05 +0530

    Added: label table seeder

---
+++ database/seeders/LabelSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Label;
+use Illuminate\Database\Seeder;
+
+class LabelSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Label::create([
+            'label'=>'exercise',
+            'title_id'=>'6'
+        ]);
+    }
+}
